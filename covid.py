c30a509458581eb6eacebefeefdf66e5c86037df
Karrthik Arya <200020068@iitb.ac.in>
2021-05-24 11:02:21 +0530

    comments added

--- covid.py
+++ covid.py
@@ -3,24 +3,24 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 URL = 'https://www.grainmart.in/news/covid-19-coronavirus-india-state-and-district-wise-tally/'
-page = requests.get(URL)
+page = requests.get(URL) #extract the webpage 
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='covid-19-table')
+soup = BeautifulSoup(page.content, 'html.parser') #create a soup object
+results = soup.find(id='covid-19-table') #div containg the data that we need
 st = input("Enter the state: ")
 st =st.title()
-states = results.find_all("div", class_ = 'skgm-states')
+states = results.find_all("div", class_ = 'skgm-states') #creates an iterable object each containg the data for a state
 for state in states:
   if(str(state).find(st) != -1):
   	state_need = state
-extract = state_need.find_all("div", class_ = 'skgm-td')
+extract = state_need.find_all("div", class_ = 'skgm-td') #get data for the state entered by the user
 districts = []
 cases = []
 for i in range(10,len(extract), 5):
-	districts.append(str(extract[i])[21:-6])
+	districts.append(str(extract[i])[21:-6]) # extract district names
 for i in range(11, len(extract), 5):
 	case = str(extract[i].find('div', class_ = 'td-dc'))
-	cases.append(case[19:-6]) 
+	cases.append(case[19:-6]) #extract no. of confirmed covid cases in each district
 data = {'Districts': districts, 'Confirmed Cases': cases}
-output = pd.DataFrame(data)
-output.to_csv('Cases in '+st +'.csv')
+output = pd.DataFrame(data) #create dataframe from the scraped data
+output.to_csv('Cases in '+st +'.csv') #export it as csv
--- groundworks.cpp
+++ groundworks.cpp
@@ -3,27 +3,32 @@
 #include<string.h>
 using namespace std;
 struct Groundwork
-{
+{	
+	//create the struct Groundwork
 	int start_time;
 	int duration;
 	int end_time;
 };
 struct Available_Groundworks
 {
+	// create struct available groundworks
 	int n;
-	Groundwork gwks[100];
+	Groundwork gwks[100]; // initialize the array with a large number assuming that n would surely be less than this
 };
 void sort(Groundwork gwks[], int n){
+	// fucntion to sort the array using selection sort
 	if(n == 1)
-		return;
+		return; // base condition
 	int min_i = 0;
 	int min = gwks[0].start_time;
 	for(int i =0;i<n;i++){
+		// find element with minimum start_time
 		if(gwks[i].start_time<min){
 			min = gwks[i].start_time;
 			min_i = i;
 		}
 		else if(gwks[i].start_time == min){
+			// the function with min duration should come first
 			if(gwks[i].duration<gwks[min_i].duration){
 				min = gwks[i].start_time;
 				min_i = i;
@@ -33,28 +38,28 @@ void sort(Groundwork gwks[], int n){
 	Groundwork temp = gwks[0];
 	gwks[0] = gwks[min_i] ;
 	gwks[min_i] = temp;
-	sort(&gwks[1],n-1);
+	sort(&gwks[1],n-1); // recursion, sort the rest of the array
 
 }
 Available_Groundworks* initialize (int start_time[], int duration[], int n){
-	Available_Groundworks* a;
-	a = new Available_Groundworks;
+	// initialize an Available_Groundworks object using start_time and duration
+	Available_Groundworks* a; 
+	a = new Available_Groundworks; // store the object in heap memory
 	a->n = n;
+	// initialize each groundowork
 	for(int i=0;i<n;i++){
 		a->gwks[i].start_time = start_time[i];
 		a->gwks[i].duration = duration[i];
 		a->gwks[i].end_time = start_time[i]+duration[i];
 	}
-	return a;
+	return a; 
 }
 int CalculateMaxGroundworks(Available_Groundworks* ptr){
+	//function to calculate maximum groundworks that someone can attend
 	int n = ptr->n;
 	int x = 1;
 	int i_last = 0;
-	sort(ptr->gwks, n);
-	for(int i=0;i<n;i++){
-		cout<<ptr->gwks[i].start_time<<" "<<ptr->gwks[i].end_time<<endl;
-	}
+	sort(ptr->gwks, n); //sorts the array on basis of start_time and then duration
 	for(int i = 1; i<n; i++){
 		if(ptr->gwks[i].start_time >= ptr->gwks[i_last].end_time){
 			x++;
@@ -65,19 +70,21 @@ int CalculateMaxGroundworks(Available_Groundworks* ptr){
 }
 
 int main(){
-	ifstream infile("Schedule.txt");
+	ifstream infile("Schedule.txt"); // read the input file
 	string line;
 	int start_time[100];
 	int duration[100];
 	int n;
 	if(infile.is_open()){
+		//iterate through each line of input file
 		 for(n =0; getline(infile,line); n++){
+		 	// extract start_time and duration from each line
 		 	start_time[n] = stoi(line.substr(5,6));
 		 	duration[n] = stoi(line.substr(8));
        	}
 	}
 	Available_Groundworks* available;
-	available = initialize(start_time, duration, n);
+	available = initialize(start_time, duration, n); // pointer to the initialized Availble_groundworks object
 	int x = CalculateMaxGroundworks(available);
-	cout << x;
+	cout << x<<endl;
 }
